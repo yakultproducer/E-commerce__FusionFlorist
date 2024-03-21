36ce6a1c1ab1b52ad9a4453486fb8e77e433fb4b
yakultproducer <johnathanwong2001@gmail.com>
2024-03-20 22:54:37 -0700

    dynamic home page

--- server.js
+++ server.js
@@ -2,17 +2,16 @@ const express = require("express")
 const app = express()
 const path = require('path');
 require('dotenv').config();
-const { Client } = require('pg');
+const { Pool } = require('pg');
 const port = process.env.PORT || 3000;
 
 
 
-const connectionString = process.env.DB_connectionString
-const client = new Client({
-    connectionString: connectionString,
+const pool = new Pool({
+    connectionString: process.env.DB_connectionString
 });
 
-// client.connect();
+
 
 // Set up Express to use EJS as the view engine
 app.set('view engine', 'ejs');
@@ -21,8 +20,9 @@ app.use(express.static('public'));
 
 app.get('/', async (req, res) => {
     // Connect to database
+    const client = await pool.connect();
     try {
-        await client.connect()
+        
         // Query the PostgreSQL items table to retrieve item data
         const queryResult = await client.query('SELECT * FROM items');
         const items = queryResult.rows;
@@ -30,10 +30,12 @@ app.get('/', async (req, res) => {
         console.log(items);
         // Render the index.ejs template with the items data
         res.render('index', { items });
-        await client.end()
     } catch (error) {
         console.error('Error fetching items:', error);
         res.status(500).send('Internal Server Error');
+    } finally {
+        client.release(); // Release the client back to the pool
+        console.log('Connection closed');
     }
     
 });
@@ -41,11 +43,6 @@ app.get('/', async (req, res) => {
 app.get('/login', (req, res) => {
     res.sendFile(path.join(__dirname, 'public/login.html'));
 });
-// app.set("view engine", "ejs")
-
-// const userRouter = require("./routes/users")
-
-// app.use("/users", userRouter)
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
